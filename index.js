c8f7024fef3ee76e1876c5cebf0bad266b6da86e
codenameransom <codejoshuaroque@gmail.com>
2021-04-01 00:43:08 -0400

    commit tree a56f4ac91c694f14c657985f7700748fb35ef28a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(activity= `roller-skate`){
+    let string = `This Saturday, I want to ${activity}!`;
+    return string;
+}
+
+let mondayWork = function(activity ="go to the office"){
+    return`This Monday, I will ${activity}.`
+}
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
